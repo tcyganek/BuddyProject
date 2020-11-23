56be4f08152e190610498812f970bfa264f71d70
Tomasz Cyganek <tomasz.cyganek@mycit.ie>
2020-11-23 22:38:12 +0000

    The first code change before buddy implementation

--- HelloPython.py
+++ HelloPython.py
@@ -3,6 +3,7 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print("This is first change on local repository world!")
 
 
 	
