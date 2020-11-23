9a9d43d3b0b3a75fce2f2463a8f4ac06cddb18ae
Tomasz Cyganek <tomasz.cyganek@mycit.ie>
2020-11-23 23:01:28 +0000

    Buddy implemented

--- HelloPython.py
+++ HelloPython.py
@@ -5,6 +5,6 @@ print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 print("This is first change on local repository world!")
 print("This is the second change on local repository world!")
-
+print("The buddy CI is implemented at this stage of project")
 
 	
