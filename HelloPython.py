4198378824e2e014811b0499868c42fe334dde88
Tomasz Cyganek <tomasz.cyganek@mycit.ie>
2020-11-23 22:47:58 +0000

    Second change in code created before buddy implementation

--- HelloPython.py
+++ HelloPython.py
@@ -4,6 +4,7 @@ now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 print("This is first change on local repository world!")
+print("This is the second change on local repository world!")
 
 
 	
