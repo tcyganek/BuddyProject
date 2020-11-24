1d5fb209f6a99c6beaac3b9a8845cd6c3009f5c7
Tomasz Cyganek <tomasz.cyganek@mycit.ie>
2020-11-24 14:24:44 +0000

    Test on Tuesday lab

--- HelloPython.py
+++ HelloPython.py
@@ -6,5 +6,5 @@ print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 print("This is first change on local repository world!")
 print("This is the second change on local repository world!")
 print("The buddy CI is implemented at this stage of project")
-
+print("Test, Tuesday 24.11.2020")
 	
