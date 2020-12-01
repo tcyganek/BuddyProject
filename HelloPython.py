ca9c8dec0976bb31bb17d03eff54a09732d9f121
Tomasz Cyganek <tomasz.cyganek@mycit.ie>
2020-12-01 14:25:27 +0000

    Test Presentation

--- HelloPython.py
+++ HelloPython.py
@@ -7,4 +7,5 @@ print("This is first change on local repository world!")
 print("This is the second change on local repository world!")
 print("The buddy CI is implemented at this stage of project")
 print("Test, Tuesday 24.11.2020")
-print("Second Test, Tuesday 24.11.2020")
+print("Second Test, Tuesday 24.11.2020").
+print(" Test, Tuesday 1.12.2020")
